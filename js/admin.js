50f36b940038ce7d05c065a9daabbd384cb39bd4
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-10-13 16:45:22 +0200

    Create admin.js

---
+++ js/admin.js
@@ -0,0 +1,34 @@
+import { baseUrl } from "./settings/api.js";
+import createMenu from "./components/createMenu.js";
+import displayMessage from "./components/displayMessage.js";
+import redirectUnauthorized from "./components/redirectUnauthorized.js";
+
+redirectUnauthorized();
+createMenu();
+
+const articleUrl = baseUrl + "articles";
+
+(async function () {
+    const container = document.querySelector("tbody");
+    const loading = document.querySelector(".loading");
+
+    try {
+        const response = await fetch(articleUrl);
+        const json = await response.json();
+
+        loading.innerHTML = "";
+        container.innerHTML = "";
+
+        json.forEach(function (article) {
+            container.innerHTML += `<tr>
+                                        <th scope="row">${article.id}</th>
+                                        <td>${article.title}</td>
+                                        <td>${article.summary}</td>
+                                        <td>${article.author}</td>
+                                        <td><a href="edit.html?id=${article.id}"><i class="nav-link fas fa-pen"></i></a></td>
+                                    </tr>`;
+        });
+    } catch (error) {
+        displayMessage("alert-danger", error, "tbody");
+    }
+})();
