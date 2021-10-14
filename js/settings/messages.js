42e0e4b7d68553afd72bdadeb30f0349652a9517
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-10-14 17:58:13 +0200

    Update messages.js

--- js/settings/messages.js
+++ js/settings/messages.js
@@ -6,3 +6,4 @@ export const SAVED_SUCCESS = "Changes successfully saved";
 export const INVALID_LOGIN_VALUE = "Please enter valid login details";
 export const WRONG_LOGIN_VALUE = "Wrong email address or password";
 export const STANDARD_ERROR = "An error occured";
+export const DELETE_CONFIRM = "Are you sure? You won't be able to recover the article.";
